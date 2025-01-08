18d50548b16dcaf7e0d1203862690f96d1b00ee3
etvx86 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2025-01-08 12:14:10 -0500

    Initial commit; work begins now.

---
+++ priism.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+clear
+
+releaseBuild=0
+recoroot="/mnt/recoroot"
+
+if [[ $releaseBuild -eq 1 ]]; then
+	trap '' INT
+fi
+
+funText() {
+	splashText=("       Triangle is love, triangle is life.", "             Placeholder splash text")
+  	selectedSplashText=${splashText[$RANDOM % ${#splashText[@]}]}
+	echo " "
+   	echo "$selectedSplashText"
+}
+
+splash() {
+	echo "                                              ...."
+	echo "                        ..                  ......"
+	echo "                       .::.              ........."
+	echo "                      .:..:.          ......:::..."
+	echo "                     .::..::.      ..::::---:::..."
+	echo "  ........          ::::::::::  ..::-====--::.... "
+	echo "        ...:::::...::::::::..:::-=++=--:.....     "
+	echo "              ....----:::::::::-:.....            "
+	echo "                .:-:.........::::.                "
+	echo "               .............::::-:.               "
+	echo "               ............::::::-:.              "
+	echo "              .....::::::::::::::--:              "
+	echo "                      Priism                      "
+	echo "                        or                        "
+	echo "  Portable recovery image installer/shim manager  "
+	echo "                      v0.9a                       "
+	funText
+	echo " "
+}
+
+splash
+echo "THIS IS A PROTOTYPE BUILD, DO NOT EXPECT EVERYTHING TO WORK PROPERLY!!!"
+
+mkdir /mnt/priism
+mkdir /mnt/new_root
+mkdir /mnt/shimroot
+mkdir /mnt/recoroot
+
+priism_images="$(cgpt find -l PRIISM_IMAGES | head -n 1 | grep --color=never /dev/)"
+mount $priism_images /mnt/priism
+
+recochoose=(/mnt/priism/recovery/*)
+shimchoose=(/mnt/priism/shims/*)
+
+shimboot() {
+	find /mnt/priism/shims -type f
+	while true; do
+		read -p "Please choose a shim to boot: " shimtoboot
+		
+		if [[ $shimtoboot == "exit" ]]
+		then
+			break
+		fi
+		
+		if [[ ! -f /mnt/priism/shims/$shimtoboot ]]
+		then
+			echo "File not found! Try again."
+		else
+			echo "Function not yet implemented."
+		fi
+	done
+	read -p "Press any key to continue"
+	losetup -D
+	splash
+}
+
+installcros() {
+	echo "Choose the image you want to flash, or type exit:"
+	select FILE in "${recochoose[@]}"; do
+ 		if [[ -n "$FILE" ]]; then
+			reco=$FILE
+			break
+		fi
+	done
+		
+	if [[ $reco == "exit" ]]; then
+		break
+	fi
+  
+	mkdir -p $recoroot
+
+	echo "Searching for ROOT-A on reco image..."
+	loop=$(losetup -fP --show $reco)
+	loop_root="$(cgpt find -l ${loop} ROOT-A | head -n 1 | grep --color=never /dev/)"
+	if mount -r "${loop_root}" $recoroot ; then
+		result=$?
+		echo "ROOT-A found successfully and mounted."
+	else
+		echo "Mount process failed! Exit code was ${result}."
+		echo "This may be a bug! Please check your recovery image,"
+		echo "and if it looks fine, report it to the GitHub repo!"
+	fi
+
+	mount -t proc /proc $recoroot/proc/
+	mount --rbind /sys $recoroot/sys/
+	mount --rbind /dev $recoroot/dev/
+
+	/mnt/recoroot/usr/sbin/chromeos-recovery $loop
+}
+
+rebootdevice() {
+	echo "Rebooting..."
+	reboot
+}
+
+shutdowndevice() {
+	echo "Shutting down..."
+	shutdown -h now
+}
+
+exitdebug() {
+	if [[ releaseBuild -eq 0 ]]; then
+		exit
+	else
+		echo "Invalid option"
+	fi
+}
+
+while true; do
+	echo "Select an option:"
+	echo "(1 or b) Bash shell"
+	echo "(2 or s) Boot an RMA shim"
+	echo "(3 or i) Install a ChromeOS recovery image"
+	echo "(4 or r) Reboot"
+	echo "(5 or p) Power off"
+	if [[ releaseBuild -eq 0 ]]; then
+		echo "(6 or e) Exit [Debug]"
+	fi
+	read -p "> " choice
+	case "$choice" in
+	1| b | B) bash ;;
+	2 | r | R) rebootdevice ;;
+	3 | p | P) shutdowndevice ;;
+	4 | s | S) shimboot ;;
+	5 | i | I) installcros ;;
+	6 | e | E) exitdebug ;;
+	*) echo "Invalid option" ;;
+	esac
+	echo ""
+done
---
+++ priism_builder.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+IMAGE=$1
+SCRIPT_DIR=$(dirname "$0")
+SCRIPT_DIR=${SCRIPT_DIR:-"."}
+. "$SCRIPT_DIR/wax_common.sh"
+
+set -eE
+
+[ "$EUID" -ne 0 ] && fail "Please run as root"
+[ -z "$1" ] && echo "Specify a sh1mmer legacy image to modify with 'priism_builder.sh image.bin'"
+
+cleanup() {
+	[ -d "$MNT_SH1MMER" ] && umount "$MNT_SH1MMER" && rmdir "$MNT_SH1MMER"
+	[ -z "$LOOPDEV" ] || losetup -d "$LOOPDEV" || :	
+	trap - EXIT INT
+}
+
+patch_sh1mmer() {
+	log_info "Creating Priism images partition ($(format_bytes $SH1MMER_PART_SIZE))"
+	local sector_size=$(get_sector_size "$LOOPDEV")
+	cgpt_add_auto "$IMAGE" "$LOOPDEV" 5 $((SH1MMER_PART_SIZE / sector_size)) -t data -l PRIISM_IMAGES
+	mkfs.ext4 -F -b 4096 -L PRIISM_IMAGES "${LOOPDEV}p5"
+	
+	safesync
+	suppress sgdisk -e "$IMAGE" 2>&1 | sed 's/\a//g'
+	safesync
+
+	MNT_SH1MMER=$(mktemp -d)
+	MNT_priism=$(mktemp -d)
+	mount "${LOOPDEV}p1" "$MNT_SH1MMER"
+
+	log_info "Copying payload"
+	cp priism.sh "$MNT_SH1MMER/root/noarch/usr/sbin/sh1mmer_main.sh"
+	chmod -R +x "$MNT_SH1MMER"
+
+	umount "$MNT_SH1MMER"
+	rmdir "$MNT_SH1MMER"
+
+	mkfs.ext4 -F -b 4096 -L PRIISM_IMAGES "${LOOPDEV}p5"
+
+	mount "${LOOPDEV}p5" "$MNT_priism"
+
+	mkdir "$MNT_priism/shims"
+	mkdir "$MNT_priism/recovery"
+	umount $MNT_priism
+	rmdir $MNT_priism
+}
+
+trap 'echo $BASH_COMMAND failed with exit code $?. THIS IS A BUG, PLEASE REPORT!' ERR
+trap 'cleanup; exit' EXIT
+trap 'echo Abort.; cleanup; exit' INT
+FLAGS_sh1mmer_part_size=64M
+		
+if [ -b "$IMAGE" ]; then
+	log_info "Image is a block device, performance may suffer..."
+else
+	check_file_rw "$IMAGE" || fail "$IMAGE doesn't exist, isn't a file, or isn't RW"
+	check_slow_fs "$IMAGE"
+fi
+
+check_gpt_image "$IMAGE" || fail "$IMAGE is not GPT, or is corrupted"
+
+SH1MMER_PART_SIZE=$(parse_bytes "$FLAGS_sh1mmer_part_size") || fail "Could not parse size '$FLAGS_sh1mmer_part_size'"
+
+sudo dd if=/dev/zero bs=1MiB of=$IMAGE conv=notrunc oflag=append count=100
+# sane backup table
+suppress sgdisk -e "$IMAGE" 2>&1 | sed 's/\a//g'
+
+log_info "Creating loop device"
+LOOPDEV=$(losetup -f)
+losetup -P "$LOOPDEV" "$IMAGE"
+safesync
+
+patch_sh1mmer
+safesync
+
+losetup -d "$LOOPDEV"
+safesync
+
+suppress sgdisk -e "$IMAGE" 2>&1 | sed 's/\a//g'
+
+log_info "Done. Have fun!"
+trap - EXIT
